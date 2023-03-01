fbb14db3ca90db87d5276b5ac0ce044a6f7d3de3
Shreyus Mehra <mehrashreyas01@gmail.com>
2023-03-01 19:22:50 +0530

    Completed

---
+++ src/components/Navbar/RightContent/RightContent.tsx
@@ -0,0 +1,24 @@
+import { Flex } from "@chakra-ui/react";
+import { User } from "firebase/auth";
+import React from "react";
+import AuthModal from "../../Modal/Auth/AuthModal";
+import AuthButtons from "./AuthButtons";
+import Icons from "./Icons";
+import UserMenu from "./UserMenu";
+
+type RightContentProps = {
+  user?: User | null;
+};
+
+const RightContent: React.FC<RightContentProps> = ({ user }) => {
+  return (
+    <>
+      <AuthModal />
+      <Flex justify="center" align="center">
+        {user ? <Icons /> : <AuthButtons />}
+        <UserMenu user={user} />
+      </Flex>
+    </>
+  );
+};
+export default RightContent;
---
+++ src/firebase/errors.ts
@@ -0,0 +1,6 @@
+export const FIRBASE_ERRORS = {
+  "Firbase: Error (auth/email-already-in-use).":
+    "A user with that email already exists",
+  "Firbase: Error (auth/user-not-found).": "Invalid email or password",
+  "Firbase: Error (auth/wrong-password": "Invalid email or password",
+};
